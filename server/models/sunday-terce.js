bfb6901c7e02676cd7467566a4a4c35958cc6060
Monica Labbao y Aspiras <ma.labbao@gmail.com>
2019-05-27 15:45:24 +0800

    made the psalter model hardcoded

--- server/models/sunday-terce.js
+++ server/models/sunday-terce.js
@@ -7,14 +7,28 @@ module.exports = function(Sundayterce) {
         paternoster: 'Pater noster, qui es in cælis, sanctificétur nomen tuum: advéniat regnum tuum: fiat volúntas tua, sicut in cælo et in terra. Panem nostrum quotidiánum da nobis hódie: et dimítte nobis débita nostra, sicut et nos dimíttimus debitóribus nostris: et ne nos indúcas in tentatiónem: sed líbera nos a malo. Amen.',
         avemaria: 'Ave María, grátia plena; Dóminus tecum: benedícta tu in muliéribus, et benedíctus fructus ventris tui Jesus. Sancta María, Mater Dei, ora pro nobis peccatóribus, nunc et in hora mortis nostræ. Amen.',
         deusAdjutorium: {
-          versicle : 'Deus ✠ in adjutórium meum inténde.',
-          response : 'Dómine, ad adjuvándum me festína',
+          versicle: 'Deus ✠ in adjutórium meum inténde.',
+          response: 'Dómine, ad adjuvándum me festína',
         },
         gloriaPatri: {
-          versicle : 'Glória Patri, et Fílio, * et Spirítui Sancto.',
-          response : 'Sicut erat in princípio, et nunc, et semper, * et in sǽcula sæculórum. Amen.',
-        }
-      }
-    })
-  }
+          versicle: 'Glória Patri, et Fílio, * et Spirítui Sancto.',
+          response: 'Sicut erat in princípio, et nunc, et semper, * et in sǽcula sæculórum. Amen.',
+        },
+        alleluia: 'Alleluia'
+      },
+      psalterium: [
+        {
+          chapter: '118[33-48]',
+          verses: [
+            {
+              desc: 'He',
+              num: '33',
+              la: 'Legem pone mihi, Dómine, viam justificatiónum tuárum: * et exquíram eam semper.',
+              en: 'Set before me for a law the way of thy justifications, O Lord: * and I will always seek after it.',
+            },
+          ],
+        },
+      ],
+    });
+  };
 };
